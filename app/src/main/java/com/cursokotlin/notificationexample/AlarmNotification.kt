e0aea035562a74f37fd09ad8e94f79d9e06d95e1
AristiDevs <aris.guimera@gmail.com>
2023-01-31 15:52:31 +0000

    notificationes programadas

---
+++ app/src/main/java/com/cursokotlin/notificationexample/AlarmNotification.kt
@@ -0,0 +1,46 @@
+package com.cursokotlin.notificationexample
+
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+
+class AlarmNotification:BroadcastReceiver() {
+
+    companion object{
+        const val NOTIFICATION_ID = 1
+    }
+
+    override fun onReceive(context: Context, p1: Intent?) {
+        createSimpleNotification(context)
+    }
+
+    private fun createSimpleNotification(context: Context) {
+        val intent = Intent(context, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+
+        val flag = if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) PendingIntent.FLAG_IMMUTABLE else 0
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(context, 0, intent, flag)
+
+        val notification = NotificationCompat.Builder(context, MainActivity.MY_CHANNEL_ID)
+            .setSmallIcon(android.R.drawable.ic_delete)
+            .setContentTitle("My title")
+            .setContentText("Esto es un ejemplo <3")
+            .setStyle(
+                NotificationCompat.BigTextStyle()
+                    .bigText("Holita holi Holita holi Holita holi Holita holi Holita holi Holita holi Holita holi Holita holi Holita holi Holita holi Holita holi Holita holi Holita holi ")
+            )
+            .setContentIntent(pendingIntent)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .build()
+
+        val manager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        manager.notify(NOTIFICATION_ID, notification)
+    }
+
+}
--- app/src/main/java/com/cursokotlin/notificationexample/MainActivity.kt
+++ app/src/main/java/com/cursokotlin/notificationexample/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.cursokotlin.notificationexample
 
+import android.app.AlarmManager
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
@@ -11,6 +12,8 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import com.cursokotlin.notificationexample.AlarmNotification.Companion.NOTIFICATION_ID
+import java.util.Calendar
 
 class MainActivity : AppCompatActivity() {
     companion object {
@@ -23,11 +26,24 @@ class MainActivity : AppCompatActivity() {
         val myNotificationButton = findViewById<Button>(R.id.btnNotification)
         createChannel()
         myNotificationButton.setOnClickListener {
-            createSimpleNotification()
+            scheduleNotification()
         }
     }
 
-    fun createChannel() {
+    private fun scheduleNotification() {
+        val intent = Intent(applicationContext, AlarmNotification::class.java)
+        val pendingIntent = PendingIntent.getBroadcast(
+            applicationContext,
+            NOTIFICATION_ID,
+            intent,
+            PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
+        )
+
+        val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        alarmManager.setExact(AlarmManager.RTC_WAKEUP, Calendar.getInstance().timeInMillis + 15000, pendingIntent)
+    }
+
+    private fun createChannel() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val channel = NotificationChannel(
                 MY_CHANNEL_ID,
@@ -41,31 +57,6 @@ class MainActivity : AppCompatActivity() {
                 getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
             notificationManager.createNotificationChannel(channel)
-        }
-    }
-
-    fun createSimpleNotification() {
-
-        val intent = Intent(this, MainActivity::class.java).apply {
-            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-        }
-
-        val flag = if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) PendingIntent.FLAG_IMMUTABLE else 0
-        val pendingIntent:PendingIntent = PendingIntent.getActivity(this, 0, intent, flag)
-
-        var builder = NotificationCompat.Builder(this, MY_CHANNEL_ID)
-            .setSmallIcon(android.R.drawable.ic_delete)
-            .setContentTitle("My title")
-            .setContentText("Esto es un ejemplo <3")
-            .setStyle(
-                NotificationCompat.BigTextStyle()
-                    .bigText("Holita holi Holita holi Holita holi Holita holi Holita holi Holita holi Holita holi Holita holi Holita holi Holita holi Holita holi Holita holi Holita holi ")
-            )
-            .setContentIntent(pendingIntent)
-            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-
-        with(NotificationManagerCompat.from(this)) {
-            notify(1, builder.build())
         }
     }
 }
